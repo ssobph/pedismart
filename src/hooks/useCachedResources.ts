e743deeae64503627208b14fe61a2aba393f0cca
Jastine Hernan <jastinehernan@gmail.com>
2025-08-31 01:16:28 +0800

    feat: add custom hook for managing cached resources and loading fonts

---
+++ src/hooks/useCachedResources.ts
@@ -0,0 +1,36 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+
+// PREVENT THE SPLASH SCREEN FROM AUTO-HIDING BEFORE ASSET LOADING IS COMPLETE.
+SplashScreen.preventAutoHideAsync();
+
+export default function useCachedResources() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const [fontsLoaded, fontError] = useFonts({
+    ...FontAwesome5.font,
+    // TODO: UPDATE FONT ASK JASTINE
+    // 'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      if (fontError) {
+        console.warn('An error occurred while loading fonts:', fontError);
+      }
+      setAppIsReady(true);
+    }
+  }, [fontsLoaded, fontError]);
+
+  // CREATE A MEMOIZED CALLBACK TO HIDE THE SPLASH SCREEN.
+  // TODO: THIS SHOULD BE PASSED TO THE `onLayout` PROP 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return { appIsReady, onLayoutRootView };
+}
